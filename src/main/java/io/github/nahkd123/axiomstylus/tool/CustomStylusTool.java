471b6f4ea625095c791bd46bb835348a926a5e40
nahkd123 <nahkd123@gmail.com>
2025-03-27 15:19:39 +0700

    Basic mouse support

--- src/main/java/io/github/nahkd123/axiomstylus/tool/CustomStylusTool.java
+++ src/main/java/io/github/nahkd123/axiomstylus/tool/CustomStylusTool.java
@@ -1,8 +1,10 @@
 package io.github.nahkd123.axiomstylus.tool;
 
 import org.joml.Matrix4f;
+import org.lwjgl.glfw.GLFW;
 
 import com.moulberry.axiomclientapi.CustomTool;
+import com.moulberry.axiomclientapi.service.ToolService;
 
 import io.github.nahkd123.axiomstylus.AxiomStylusAddon;
 import io.github.nahkd123.axiomstylus.brush.BrushPoint;
@@ -16,14 +18,38 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.Vec3d;
 
+/**
+ * <p>
+ * An abstract class for tool implementation that uses graphics tablet as input.
+ * </p>
+ */
 public abstract class CustomStylusTool implements CustomTool {
 	private BrushStrokeImpl stroke = null;
 	private ImmutableBrushPoint prevPoint = null;
 
+	// Emulate input
+	private InputEmulateMode inputEmulateMode = null;
+
 	@Override
 	public void render(Camera camera, float tickDelta, long time, MatrixStack poseStack, Matrix4f projection) {
-		// TODO handle mouse input
-		InputReport input = TabletManager.get(MinecraftClient.getInstance()).getLastReport();
+		InputReport input;
+
+		if (inputEmulateMode == InputEmulateMode.MOUSE) {
+			ToolService toolService = AxiomStylusAddon.TOOL_SERVICE;
+			boolean holdingButton = toolService.isMouseDown(GLFW.GLFW_MOUSE_BUTTON_LEFT)
+				|| toolService.isMouseDown(GLFW.GLFW_MOUSE_BUTTON_RIGHT);
+
+			if (holdingButton) {
+				input = new InputReport(InputReport.STATUS_TIP_TOUCHING, 0, 0f, 0f, 1f, 0f, 0f);
+			} else {
+				input = new InputReport(0, 0, 0f, 0f, 0f, 0f, 0f);
+				inputEmulateMode = null;
+			}
+		} else {
+			input = TabletManager.get(MinecraftClient.getInstance()).getLastReport();
+		}
+
+		// TODO handle input in graphics tablet thread
 		boolean wasDrawing = stroke != null;
 		boolean drawing = input != null && input.tipTouching();
 		BlockHitResult raycast = AxiomStylusAddon.TOOL_SERVICE.raycastBlock();
@@ -44,11 +70,13 @@ public abstract class CustomStylusTool implements CustomTool {
 			stroke.totalDistance += next.position().distanceTo(prevPoint.position());
 			onBrushInput(stroke, prevPoint, next);
 			prevPoint = next;
-		} else {
-			// TODO brush outline?
 		}
+	}
 
-		wasDrawing = drawing;
+	@Override
+	public boolean callUseTool() {
+		inputEmulateMode = InputEmulateMode.MOUSE;
+		return true;
 	}
 
 	private ImmutableBrushPoint inputToBrushPoint(long ts, Vec3d position, InputReport report) {
@@ -81,5 +109,9 @@ public abstract class CustomStylusTool implements CustomTool {
 		public float jitter() {
 			return jitter;
 		}
+	}
+
+	private enum InputEmulateMode {
+		MOUSE;
 	}
 }
